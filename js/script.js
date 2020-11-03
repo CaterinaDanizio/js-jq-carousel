d828ddfb1f22ee7384ab8a7f3e5c3d5909abaadf
Caterina <danizio@outlook.it>
2020-11-03 19:15:07 +0100

    Function prev & next with arrow left and right keydown

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $( document ).ready( function() {
 
-  $(".next").click(nextImg);
+  $(".next").click(nextImg)
   $(".prev").click(prevImg);
 
   // FUNZIONI
 
-  // funzione per l'avanti
+  // funzione per avanti
   function nextImg() {
     //console.log("hai cliccato su next!!");
 
@@ -22,6 +22,7 @@ $( document ).ready( function() {
       imgAttiva.next("img").addClass("active");
     }
  }
+ // funzione per indietro
 
  function prevImg() {
    var imgAttiva = $("img.active");
@@ -34,7 +35,36 @@ $( document ).ready( function() {
      imgAttiva.prev("img").addClass("active");
    }
  }
-
-
-
 });
+
+// funzione per tasto arrow left
+$("body").keydown(function keyNext(e)
+{
+    // left arrow
+    if ((e.keyCode || e.which) == 37)
+    {
+      var imgAttiva = $("img.active");
+      imgAttiva.removeClass("active");
+      if (imgAttiva.hasClass("first")){
+        //torna alla prima img
+        $("img.last").addClass("active");
+      } else {// altrimenti
+        //fai diventare attiva l'immagine successiva
+        imgAttiva.prev("img").addClass("active");
+      }
+    }
+    // right arrow
+    if ((e.keyCode || e.which) == 39)
+    {
+      var imgAttiva = $("img.active");
+      imgAttiva.removeClass("active");
+      // se dove sono è l'ultima img allora
+      if (imgAttiva.hasClass("last")){
+        //torna alla prima img
+        $("img.first").addClass("active");
+        } else { // altrimenti
+        //fai diventare attiva l'immagine successiva
+        imgAttiva.next("img").addClass("active");
+      }
+    }
+  });
